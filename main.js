4b8b867e2c5fde435213d290bcc7b0dc5fe666a9
Anurag Kaushik <anurag11b12345@gmail.com>
2024-03-10 23:32:21 +0530

    chnages

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*--------------NAVBAR scroll -----------*/
 
 window.addEventListener(`scroll`, () => {
-    document.querySelector(`nav`).classList.toggle(`window-scroll`, window.scrollY > 20);
+    document.querySelector(`nav`).classList.toggle(`window-scroll`, window.scrollY > 50);
 })
