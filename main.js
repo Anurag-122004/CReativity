258bdf2aaf64cd9ecf84a32b17249ca6488df1a1
Anurag Kaushik <anurag11b12345@gmail.com>
2024-03-11 00:53:36 +0530

    new update

--- main.js
+++ main.js
@@ -3,3 +3,19 @@
 window.addEventListener(`scroll`, () => {
     document.querySelector(`nav`).classList.toggle(`window-scroll`, window.scrollY > 50);
 })
+
+const faqs = document.querySelectorAll(`.faq`);
+
+faqs.forEach(faq => {
+    faq.addEventListener(`click`, () => {
+        faq.classList.toggle(`open`);
+
+        const icon = faq.querySelector(`.faq__icon i`);
+
+        if (icon && icon.className.includes('fa-plus')) {
+            icon.className = 'fa-solid fa-minus';
+        } else {
+            icon.className = 'fa-regular fa-plus';
+        }
+    })
+})
